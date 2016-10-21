372251cf5b19b228de7370ca7c4bc35a7e50eea0
Daniel Dinnyes <ddinnyes@gmail.com>
2016-10-21 20:56:14 +0100

    major update and fixes/refactoring to booking app
    - changed booking to POST based form submittal
    - added error reporting to bookings without name
    - cleaned up html generation code
    - added nice start page, and navigation links between pages

--- src/dojo_appointment/core.clj
+++ src/dojo_appointment/core.clj
@@ -15,16 +15,23 @@
   TableRow
   (getrow [this]
     (x/element :tr {}
-      [(x/element :td {} (:start this))
-       (x/element :td {} (:end this))])))
+      [(x/element :td {} start)
+       (x/element :td {} end)
+       (x/element :td {}
+         (x/element :form {:method "post" :action "/bookapt"}
+           (x/element :input {:type "text" :name "name"})
+           (x/element :input {:type "hidden" :name "begin" :value start})
+           (x/element :input {:type "hidden" :name "end" :value end})
+           (x/element :input {:type "submit" :value "Book"})))])))
 
 (defrecord Booking [slot name]
   TableRow
   (getrow [this]
-    (x/element :tr {}
-      [(x/element :td {} (:start (:slot this)))
-       (x/element :td {} (:end (:slot this)))
-       (x/element :td {} (:name this))])))
+    (let [{:keys [start end]} slot]
+      (x/element :tr {}
+        [(x/element :td {} start)
+         (x/element :td {} end)
+         (x/element :td {} name)]))))
 
 (defrecord BookingSystem [free booked])
 
@@ -50,21 +57,21 @@
       (throw (Exception. "No such slot exists")))))
 
 (defn book-apt [start end name]
-  (dosync
-    (alter appts with-new start end name)))
+  (if (not (empty? name))
+    (dosync
+      (alter appts with-new start end name))
+    (throw (Exception. "Name was empty"))))
 
 (defn elempara [text]
-  (x/element :p {"class" "example"} text))
+  (x/element :p {} text))
 
-(defn htmldoc [title content]
+(defn htmldoc [title & content]
   (x/element :html {}
     (x/element :head {}
       (x/element :title {} title))
     (x/element :body {}
+      (x/element :h1 {} title)
       content)))
-
-(defn simpletext-html [title & paragraphs]
-  (htmldoc title (map elempara paragraphs)))
 
 (defn elem-table [header rows]
   "Takes header (list of strings) and rows (list of arrays of string elements)
@@ -75,30 +82,45 @@ and outputs a html table element using these."
     (map getrow rows)))
 
 (defroutes app
-  (GET "/freeslots" []
-    (println "Received message for freeslots")
+  (GET "/" []
+    (x/emit-str
+      (htmldoc "Appointment Booking System"
+        (elempara
+          (x/element :a {:href "/slots"} "View available free slots"))
+        (elempara
+          (x/element :a {:href "/bookings"} "View all existing bookings")))))
+  (GET "/slots" []
     (x/emit-str
       (htmldoc "Appointment Free Slots"
-        (elem-table ["Begin" "End"] (getfreeslots)))))
-  (GET "/bookings" req
-    (println "Received message for bookings")
+        (elem-table ["Begin" "End" "Name"] (getfreeslots))
+        (elempara
+          (x/element :a {:href "/bookings"} "View all existing bookings")))))
+  (GET "/bookings" []
     (x/emit-str
       (htmldoc "Booked Appointments"
-        (elem-table ["Start" "End" "Name"] (getbookings)))))
-  (GET "/bookapt" {{:strs [begin end name]} :query-params}
-    (println "Received message to bookapt:" begin "," end "," name)
+        (elem-table ["Start" "End" "Name"] (getbookings))
+        (elempara
+          (x/element :a {:href "/slots"} "View free slots to book")))))
+  (POST "/bookapt" [begin end name]
     (try 
       (book-apt (Integer. begin) (Integer. end) name)
       (x/emit-str
-        (simpletext-html
-          "Appointment Booking"
-          "Appointment successfully booked!"))
+        (htmldoc "Appointment Booking"
+          (elempara
+            "Appointment successfully booked!")
+          (elempara
+            (x/element :a {:href "/bookings"} "View all booked appointments"))
+          (elempara
+            (x/element :a {:href "/slots"} "Book another free slot"))))
       (catch Exception x
         (x/emit-str
-          (simpletext-html
-            "Appointment Booking"
-            "Something went wrong"
-            (.getMessage x)))))))
+          (htmldoc "Appointment Booking Error"
+            (elempara
+              "Something went wrong!")
+            (elempara
+              (.getMessage x))
+            (elempara
+              (x/element :a {:href "/slots"} "Try booking another free slot"))))))))
 
 (def handler
   (-> app wrap-params))
